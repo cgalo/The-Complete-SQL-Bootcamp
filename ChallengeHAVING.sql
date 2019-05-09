2cf490a8431250631552d722d305aec78883b6bf
cgalo <cgalo13@gmail.com>
2019-05-08 21:49:43 -0400

    Update ChallengeHAVING.sql
    Fixed typos in comment block

--- ChallengeHAVING.sql
+++ ChallengeHAVING.sql
@@ -1,7 +1,7 @@
 /* 
-Problem: We want to know what costumer are eligble for our premium platinum credit card.
-The requirements are that the costumer has at least a total of 40 transaction payments.
-What costumer are eligble for the credit card?
+Problem: We want to know what customer are eligble for our premium platinum credit card.
+The requirements are that the customer has at least a total of 40 transaction payments.
+What customer are eligble for the credit card?
 */
 
 SELECT customer_id, COUNT(amount)
